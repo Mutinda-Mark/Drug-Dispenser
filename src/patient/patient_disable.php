ab5100abfc04e42f8e04140c6113c0f880e3ed0e
Mark Mutinda <122874277+Mutinda-Mark@users.noreply.github.com>
2024-10-02 21:25:06 +0300

    Update patient_disable.php

--- src/patient/patient_disable.php
+++ src/patient/patient_disable.php
@@ -4,7 +4,7 @@ session_start(); // Start the session
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "example";
+$database = "CAT1";
 
 // Create a new connection
 $conn = new mysqli($servername, $username, $password, $database);
