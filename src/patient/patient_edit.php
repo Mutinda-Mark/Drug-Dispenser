d4ecff516d9614b091f6b69365d5213a77a59a56
Mark Mutinda <122874277+Mutinda-Mark@users.noreply.github.com>
2024-10-02 21:25:35 +0300

    Update patient_edit.php

--- src/patient/patient_edit.php
+++ src/patient/patient_edit.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "example";
+$database = "CAT1";
 
 // Create a new connection
 $conn = mysqli_connect($servername, $username, $password, $database);
